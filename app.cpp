b261fcd4bf4f5107bdac7845e976c07987495b8e
RamonUnch <74856804+RamonUnch@users.noreply.github.com>
2022-01-11 21:40:54 +0100

    MyGetVersionEx improvement thanks to Roy!

---
+++ app.cpp
@@ -0,0 +1,316 @@
+#include "stdafx.h"
+#include "app.h"
+#include "log.h"
+#include "memory.h"
+#include "thread.h"
+#include "window.h"
+#include "string.h"
+using namespace ki;
+
+
+typedef DWORD (WINAPI * Initialize_funk)(LPVOID r);
+static DWORD MyOleInitialize(LPVOID r)
+{
+	static Initialize_funk func = (Initialize_funk)(-1);
+	if (func == (Initialize_funk)(-1)) // First time!
+		func = (Initialize_funk)GetProcAddress(GetModuleHandleA("OLE32.DLL"), "OleInitialize");
+
+	if (func) { // We got the function!
+		return func(r);
+	}
+	return 666; // Fail with 666 error.
+}
+
+typedef void (WINAPI * UnInitialize_funk)( );
+static void MyOleUninitialize( )
+{
+	static UnInitialize_funk func = (UnInitialize_funk)(-1);
+	if (func == (UnInitialize_funk)(-1)) // First time!
+		func = (UnInitialize_funk)GetProcAddress(GetModuleHandleA("OLE32.DLL"), "OleUninitialize");
+
+	if (func) { // We got the function!
+		func();
+	}
+}
+typedef DWORD (WINAPI * CoCreateInstance_funk)(REFCLSID , LPUNKNOWN , DWORD , REFIID , LPVOID *);
+DWORD MyCoCreateInstance(REFCLSID rclsid, LPUNKNOWN pUnkOuter, DWORD dwClsContext, REFIID riid, LPVOID *ppv)
+{
+	static CoCreateInstance_funk func = (CoCreateInstance_funk)(-1);
+	if (func == (CoCreateInstance_funk)(-1)) // First time!
+		func = (CoCreateInstance_funk)GetProcAddress(GetModuleHandleA("OLE32.DLL"), "CoCreateInstance");
+
+	if (func) { // We got the function!
+		return func(rclsid, pUnkOuter, dwClsContext, riid, ppv);
+	}
+	return 666; // Fail with 666 error
+}
+
+#if defined(TARGET_VER)
+typedef BOOL (WINAPI *GetVersionEx_funk)(LPOSVERSIONINFOA s_osVer);
+static BOOL MyGetVersionEx(LPOSVERSIONINFOA s_osVer)
+{
+	// Try first to get the real GetVersionEx function
+	// We use the ANSI version because it does not matter.
+#if TARGET_VER > 300
+	GetVersionEx_funk func = (GetVersionEx_funk)
+		GetProcAddress(GetModuleHandleA("KERNEL32.DLL"), "GetVersionExA");
+	if (func && func( s_osVer )) 
+	{
+		if (s_osVer->dwPlatformId == VER_PLATFORM_WIN32_WINDOWS) 
+		{   // fixup broken build number in early 9x builds (roytam1)
+			s_osVer->dwBuildNumber &= 0xffff;
+		}
+		return TRUE;
+	}
+#endif
+	// Fallback in case the above failed (WinNT 3.1 / Win32s)
+	DWORD dwVersion = ::GetVersion();
+
+	s_osVer->dwMajorVersion = (DWORD)(LOBYTE(LOWORD(dwVersion)));
+	s_osVer->dwMinorVersion = (DWORD)(HIBYTE(LOWORD(dwVersion)));
+
+	if (dwVersion < 0x80000000) 
+	{ // WINNT => build number = HIWORD(dwversion)
+		s_osVer->dwBuildNumber = (DWORD)(HIWORD(dwVersion));
+		s_osVer->dwPlatformId = VER_PLATFORM_WIN32_NT;
+	}
+
+	if (s_osVer->dwPlatformId != VER_PLATFORM_WIN32_NT) 
+	{
+		if (s_osVer->dwMajorVersion == 3) s_osVer->dwPlatformId = VER_PLATFORM_WIN32s;
+		else s_osVer->dwPlatformId = VER_PLATFORM_WIN32_WINDOWS;
+	}
+
+	return TRUE;
+}
+#else
+	#define MyGetVersionEx GetVersionExA
+#endif
+
+//=========================================================================
+
+App* App::pUniqueInstance_;
+
+inline App::App()
+	: exitcode_    (-1)
+	, loadedModule_(0)
+	, hInst_       (::GetModuleHandle(NULL))
+	, hOle32_      ((HINSTANCE)(-1))
+{
+	// 唯一のインスタンスは私です。
+	pUniqueInstance_ = this;
+}
+
+#pragma warning( disable : 4722 ) // 警告：デストラクタに値が戻りません
+App::~App()
+{
+	// ロード済みモジュールがあれば閉じておく
+#if !defined(TARGET_VER) || (defined(TARGET_VER) && TARGET_VER>300)
+//	if( loadedModule_ & COM )
+//		::MyCoUninitialize();
+	if( loadedModule_ & OLE )
+		::MyOleUninitialize();
+#endif
+
+	// 終～了～
+	::ExitProcess( exitcode_ );
+}
+
+inline void App::SetExitCode( int code )
+{
+	// 終了コードを設定
+	exitcode_ = code;
+}
+
+void App::InitModule( imflag what )
+{
+	if (hOle32_ == (HINSTANCE)(-1) && what&(OLE|COM|OLEDLL)) 
+		hOle32_ = ::LoadLibraryA("OLE32.DLL");
+
+	// 初期化済みでなければ初期化する
+	bool ret = true;
+	if( !(loadedModule_ & what) )
+		switch( what )
+		{
+		case CTL:
+			::InitCommonControls(); break;
+#if !defined(TARGET_VER) || (defined(TARGET_VER) && TARGET_VER>300)
+		case COM: 
+			// Actually we only ever use OLE, that calls COM, so it can
+			// be Ignored safely...
+			//ret = S_OK == ::MyCoInitialize( NULL );
+			//MessageBoxA(NULL, "CoInitialize", ret?"Sucess": "Failed", MB_OK);
+			//break;
+		case OLE: 
+			ret = S_OK == ::MyOleInitialize( NULL );
+			// MessageBoxA(NULL, "OleInitialize", ret?"Sucess": "Failed", MB_OK);
+			break;
+#endif
+		default: break;
+		}
+
+	// 今回初期化したモノを記憶
+	if (ret) loadedModule_ |= what;
+}
+
+void App::Exit( int code )
+{
+	// 終了コードを設定して
+	SetExitCode( code );
+
+	// 自殺
+	this->~App();
+}
+
+
+
+//-------------------------------------------------------------------------
+
+const OSVERSIONINFOA& App::osver()
+{
+	static OSVERSIONINFOA s_osVer;
+	if( s_osVer.dwOSVersionInfoSize == 0 )
+	{
+		// 初回だけは情報取得
+		s_osVer.dwOSVersionInfoSize = sizeof( s_osVer );
+		MyGetVersionEx( &s_osVer );
+	}
+	return s_osVer;
+}
+
+int App::getOSVer()
+{
+	static const OSVERSIONINFOA& v = osver();
+	return v.dwMajorVersion*100+v.dwMinorVersion;
+}
+
+int App::getOSBuild()
+{
+	static const OSVERSIONINFOA& v = osver();
+	return v.dwBuildNumber;
+}
+
+bool App::isNewTypeWindows()
+{
+	static const OSVERSIONINFOA& v = osver();
+	return (
+		( v.dwPlatformId==VER_PLATFORM_WIN32_NT && v.dwMajorVersion>=5 )
+	 || ( v.dwPlatformId==VER_PLATFORM_WIN32_WINDOWS &&
+	          v.dwMajorVersion*100+v.dwMinorVersion>=410 )
+	);
+}
+
+bool App::isWin95()
+{
+	static const OSVERSIONINFOA& v = osver();
+	return (
+		v.dwPlatformId==VER_PLATFORM_WIN32_WINDOWS &&
+		v.dwMajorVersion==4 &&
+		v.dwMinorVersion==0
+	);
+}
+
+bool App::isNT()
+{
+	static const OSVERSIONINFOA& v = osver();
+	return v.dwPlatformId==VER_PLATFORM_WIN32_NT;
+}
+
+bool App::isWin32s()
+{
+	static const OSVERSIONINFOA& v = osver();
+	return v.dwPlatformId==VER_PLATFORM_WIN32s;
+}
+
+bool App::isNewShell()
+{
+	static const OSVERSIONINFOA& v = osver();
+	return v.dwMajorVersion>3;
+}
+
+bool App::is351p()
+{
+	static const OSVERSIONINFOA& v = osver();
+	return v.dwMajorVersion>3 
+		|| (v.dwMajorVersion==3 && v.dwMinorVersion >= 51);
+}
+
+bool App::isNT31()
+{
+	static const OSVERSIONINFOA& v = osver();
+	return v.dwMajorVersion==3 && v.dwMinorVersion < 50;
+}
+
+//=========================================================================
+
+extern int kmain();
+
+namespace ki
+{
+	void APIENTRY Startup()
+	{
+		// Startup :
+		// プログラム開始すると、真っ先にここに来ます。
+
+		// C++のローカルオブジェクトの破棄順序の仕様に
+		// 自信がないので(^^;、スコープを利用して順番を強制
+		// たぶん宣言の逆順だとは思うんだけど…
+
+		LOGGER( "StartUp" );
+		App myApp;
+		{
+			LOGGER( "StartUp app ok" );
+			ThreadManager myThr;
+			{
+				LOGGER( "StartUp thr ok" );
+				MemoryManager myMem;
+				{
+					LOGGER( "StartUp mem ok" );
+					IMEManager myIME;
+					{
+						LOGGER( "StartUp ime ok" );
+						String::LibInit();
+						{
+							const int r = kmain();
+							myApp.SetExitCode( r );
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#ifdef __GNUC__
+  #ifdef SUPERTINY
+	extern "C" void WINAPI entryp() { ki::Startup(); }
+  #endif
+	extern "C" void __cdecl __cxa_pure_virtual(void) {};
+	extern "C" void __deregister_frame_info() {};
+	extern "C" void __register_frame_info() {};
+	extern int __stack_chk_guard = 696115047 ;
+	extern "C" int __stack_chk_fail(){ MessageBoxA(NULL, "__stack_chk_fail", NULL, 0) ; ExitProcess(1); };
+#endif
+
+#ifdef SUPERTINY
+
+	extern "C" int __cdecl _purecall(){return 0;}
+	#ifdef _DEBUG
+		int main(){return 0;}
+	#endif
+	#pragma comment(linker, "/entry:\"Startup\"")
+
+#else
+
+	// VS2005でビルドしてもWin95で動くようにするため
+	#if _MSC_VER >= 1400
+		extern "C" BOOL WINAPI _imp__IsDebuggerPresent() { return FALSE; }
+	#endif
+
+	int APIENTRY WinMain( HINSTANCE, HINSTANCE, LPSTR, int )
+	{
+		ki::Startup();
+		return 0;
+	}
+
+#endif
