6ab8e0d4210ae5ad1d96792c40dfb8969d57f266
RamonUnch <74856804+RamonUnch@users.noreply.github.com>
2022-02-15 11:44:41 +0100

    Update to 1.13

--- GpMain.cpp
+++ GpMain.cpp
@@ -147,8 +147,8 @@ LRESULT GreenPadWnd::on_message( UINT msg, WPARAM wp, LPARAM lp )
 			pt.y -= wnd.top;
 
 			// Adjust rects so that it does not include SB nor scrollbars.
-			nc->rgrc[2].right  -= Max(24, (wnd.right-wnd.left) - nc->lppos->cx+24);
-			nc->rgrc[2].bottom -= Max(45, (wnd.bottom-wnd.top) - nc->lppos->cy+45);
+			nc->rgrc[2].right  -= Max(24L, (wnd.right-wnd.left) - nc->lppos->cx+24);
+			nc->rgrc[2].bottom -= Max(45L, (wnd.bottom-wnd.top) - nc->lppos->cy+45);
 
 			// Do not include caption+menu in BitBlt
 			nc->rgrc[1].left = nc->lppos->x + pt.x;
@@ -162,11 +162,12 @@ LRESULT GreenPadWnd::on_message( UINT msg, WPARAM wp, LPARAM lp )
 		}break;
 
 	case WM_ERASEBKGND:{
-//		// Uncomment to see in black the area that will be repainted
+		// Uncomment to see in black the area that will be repainted
+//		Sleep(200);
 //		RECT rc;
 //		GetClientRect(hwnd(), &rc);
-//		Sleep(100);
 //		FillRect((HDC)wp, &rc,  (HBRUSH)GetStockObject(BLACK_BRUSH));
+//		Sleep(200);
 		return 1;
 		}break;
 
